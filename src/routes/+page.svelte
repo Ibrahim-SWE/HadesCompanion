9e3208266967a7b397584371aad6d3da7f27afc7
Ibrahim-SWE <40331543+Ibrahim-SWE@users.noreply.github.com>
2026-05-27 19:55:32 -0400

    .gitignore added. basic empty page added.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="mainContainer bg-[#18230F] min-w-full min-h-dvh flex">
+  <div class="topBanner flex flex-row">
+    <div></div>
+  </div>
+  <div class="contentContainer"></div>
+  <div class="footer"></div>
+</div>
