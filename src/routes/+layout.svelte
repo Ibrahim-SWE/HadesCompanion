08df5159c7b52c0881e3bfc45090a7017923f530
Ibrahim-SWE <40331543+Ibrahim-SWE@users.noreply.github.com>
2026-05-28 23:09:16 -0400

    added basic navbar and layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div
+  class="mainContainer bg-[#091202] min-w-full min-h-dvh flex flex-col items-center"
+>
+  <div
+    class="navBar gap-9 flex bg-[#122106] min-w-full justify-center p-2 border-t border-b border-[#538067]"
+  >
+    <div class="titleContainer flex flex-col justify-center">
+      <div class="logo"></div>
+      <div class="siteTitle">Hades Companion</div>
+    </div>
+    <div class="barItem home">Home</div>
+    <div class="barItem gods">Gods</div>
+    <div class="barItem boons">Boons</div>
+    <div class="barItem cards">Arcana Cards</div>
+    <div class="barItem weapons">Weapons</div>
+    <div class="barItem keepsakes">Keepsakes</div>
+  </div>
+  <div class="contentContainer">{@render children()}</div>
+  <div class="footer"></div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,2 @@
 <script>
 </script>
-
-<div class="mainContainer bg-[#18230F] min-w-full min-h-dvh flex">
-  <div class="topBanner flex flex-row">
-    <div></div>
-  </div>
-  <div class="contentContainer"></div>
-  <div class="footer"></div>
-</div>
