43e374ac7aba787f80aa2a51194835034a3009ec
Ibrahim-SWE <40331543+Ibrahim-SWE@users.noreply.github.com>
2026-06-08 17:14:00 -0400

    Basic gods page. Basic weapons page.

--- src/routes/hades2/gods/+page.svelte
+++ src/routes/hades2/gods/+page.svelte
@@ -1,1 +1,43 @@
-<h1 class="text-center text-textLight">Gods</h1>
+<script lang="ts">
+  import Container from "$lib/components/Container.svelte";
+  import godsData from "$lib/data/gods.json";
+  import { resolve } from "$app/paths";
+
+  type godsKeepsakeAndCurses = { keepsake: string; curses: string[] };
+
+  type GodDetails = {
+    title: string;
+    hades_1: godsKeepsakeAndCurses | null;
+    hades_2: godsKeepsakeAndCurses | null;
+  };
+  let gods: Record<string, GodDetails> = godsData;
+  const sortedGodsArray: [string, GodDetails][] = Object.entries(gods).sort(
+    (a, b) => a[0].localeCompare(b[0]),
+  );
+</script>
+
+<Container>
+  <div class="godsContainer grid grid-cols-1 sm:grid-cols-3">
+    {#each sortedGodsArray as [god, godData] (god)}
+      <div
+        class="godContainer p-2 m-2 border border-white/20 rounded-xl text-textLight"
+      >
+        <a href={resolve(`/hades2/gods/${god}`)}>
+          <div class="godandtitleContainer">
+            <h1 class="godName text-textDark text-2xl">{god}</h1>
+            <h2 class="godTitle">{godData.title}</h2>
+          </div>
+        </a>
+        <div
+          class="cursesContainer flex flex-col border-t mt-2 border-white/20"
+        >
+          <h2 class="text-textDark">Curses:</h2>
+          {#each godData.hades_2?.curses as curse (curse)}
+            <a href={resolve(`/hades2/curses/${curse}`)}>{curse}</a>
+          {/each}
+        </div>
+        <div class="keepsakeContainer"></div>
+      </div>
+    {/each}
+  </div>
+</Container>
--- src/routes/hades2/weapons/+page.svelte
+++ src/routes/hades2/weapons/+page.svelte
@@ -1,1 +1,121 @@
-<h1 class="text-center text-textLight">Weapons</h1>
+<script lang="ts">
+  import Container from "$lib/components/Container.svelte";
+  import weaponsData from "$lib/data/hades2/weapons.json";
+
+  const upgradeImages = import.meta.glob("$lib/assets/weaponupgrades/*.webp", {
+    eager: true,
+    import: "default",
+  }) as Record<string, string>;
+
+  const miscImages = import.meta.glob("/src/lib/assets/misc/*.webp", {
+    eager: true,
+    import: "default",
+  }) as Record<string, string>;
+
+  type RichTextNode = {
+    type: "text_normal" | "text_bold" | "image";
+    value?: string;
+    name?: string;
+    img_path?: string;
+  };
+
+  type WeaponUpgrade = {
+    name: string;
+    description_rich: RichTextNode[];
+    image_path: string;
+  };
+
+  type EffectRank = {
+    effect_value: string;
+    upgrade_cost: Record<string, number> | string;
+  };
+
+  type WeaponAspect = {
+    name: string;
+    description_rich: RichTextNode[];
+    effect: string;
+    effect_ranks: EffectRank[];
+  };
+
+  type WeaponDetails = {
+    entry: string;
+    image_path: string;
+    upgrades: WeaponUpgrade[];
+    aspects: WeaponAspect[];
+  };
+
+  type WeaponsDatabase = Record<string, WeaponDetails>;
+  const weapons = weaponsData as WeaponsDatabase;
+</script>
+
+<Container>
+  <div class="weaponsContainer text-textLight">
+    {#each Object.entries(weapons) as [weaponName, details] (weaponName)}
+      <div class="weaponContainer p-2 border border-white/20">
+        <div class="infoContainer p-2">
+          <h1 class="text-textDark text-2xl">{weaponName}</h1>
+          <!-- <h2 class="ml-4">Book of Shadows Entry:</h2> -->
+          <p class="ml-6 italic font-extralight text-sm indent-5">
+            {details.entry}
+          </p>
+        </div>
+        <h2 class="text-center border-t border-white/20 pt-0.5 pb-0.5">
+          Upgrades
+        </h2>
+        <div
+          class="upgradesContainer grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6"
+        >
+          {#each details.upgrades as upgrade (upgrade.name)}
+            <div
+              class="upgradeContainer bg-black/40 grid grid-cols-[1fr_1.5fr] justify-items-center items-center border border-white/20 p-1 rounded-xl"
+            >
+              <img
+                src={upgradeImages[
+                  `/src/lib/assets/weaponupgrades/${upgrade.image_path}`
+                ]}
+                alt="Upgrade Icon"
+                class="inline-block h-[3.4em] w-auto object-contain align-middle outline-2 -outline-offset-2 outline-white/30 rounded-2xl"
+              />
+              <h1 class="upgradeName justify-self-start text-xl">
+                {upgrade.name}
+              </h1>
+              <div
+                class="descriptionContainer ml-3 mr-3 mt-2 text-center col-span-2 italic font-light text-sm"
+              >
+                {#each upgrade.description_rich as part, i (i)}
+                  {#if part.type === "text_normal"}
+                    <span>{part.value}</span>
+                  {/if}
+                  {#if part.type === "text_bold"}
+                    <span class="font-bold">{part.value}</span>
+                  {/if}
+                  {#if part.type === "image"}
+                    <span class="inline-block">
+                      <img
+                        src={miscImages[
+                          `/src/lib/assets/misc/${part.img_path}`
+                        ]}
+                        alt={part.name}
+                        class="inline-block h-[1.4em] w-auto object-contain align-middle"
+                      />
+                    </span>
+                  {/if}
+                {/each}
+              </div>
+            </div>
+          {/each}
+        </div>
+        <h2 class="text-center border-t border-white/20 pt-0.5 pb-0.5">
+          Aspects
+        </h2>
+        <div class="aspectsContainer">
+          {#each details.aspects as aspect (aspect)}
+            <div class="aspectContainer">
+              <h2>{aspect.name}</h2>
+            </div>
+          {/each}
+        </div>
+      </div>
+    {/each}
+  </div>
+</Container>
