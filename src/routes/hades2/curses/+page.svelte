bad98725ae3e5ff10595023e496e13d1fbc9eb33
Ibrahim-SWE <40331543+Ibrahim-SWE@users.noreply.github.com>
2026-06-10 13:51:45 -0400

    basic curses page added.

--- src/routes/hades2/curses/+page.svelte
+++ src/routes/hades2/curses/+page.svelte
@@ -1,1 +1,30 @@
-<h1 class="text-center text-textLight">Curses</h1>
+<script lang="ts">
+  import Container from "$lib/components/Container.svelte";
+  import cursesData from "$lib/data/hades2/curses.json";
+
+  type curseDetails = {
+    name: string;
+    effect: string;
+    duration: string;
+    source: string;
+  };
+
+  //   const curses = Object.entries(cursesData) as [string, curseDetails][];
+  const curses: Record<string, curseDetails> = cursesData;
+</script>
+
+<Container>
+  <div
+    class="cursesContainer grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 text-textLight"
+  >
+    {#each Object.entries(curses) as [curse, details] (curse)}
+      <div
+        class="curseContainer text-sm m-2 p-3 border rounded-xl border-white/20"
+      >
+        <h1 class="text-textDark text-2xl">{curse}</h1>
+        <p class="">{details.effect}</p>
+        <p>{details.duration}</p>
+      </div>
+    {/each}
+  </div>
+</Container>
