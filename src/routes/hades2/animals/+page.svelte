7a3d50f2c1e7d911c03ddb31454f173aee590ab5
Ibrahim-SWE <40331543+Ibrahim-SWE@users.noreply.github.com>
2026-06-11 10:33:20 -0400

    renamed pets folder. imported assets and data to pets page.

--- src/routes/hades2/animals/+page.svelte
+++ src/routes/hades2/animals/+page.svelte
@@ -1,1 +1,15 @@
-<h1 class="text-center text-textLight">Animal Familiars</h1>
+<script lang="ts">
+  import Container from "$lib/components/Container.svelte";
+  import petsData from "$lib/data/hades2/pets.json";
+
+  const petImages = import.meta.glob("$lib/assets/pets/*.webp", {
+    eager: true,
+    import: "default",
+  }) as Record<string, string>;
+</script>
+
+<Container>
+  <div class="petsContainer">
+    <div class="petContainer"></div>
+  </div>
+</Container>
